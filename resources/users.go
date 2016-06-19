abf3f788e934ed0fa5692826ec5f2822dcb60352
Rogerio Araujo <rogerio.araujo@gmail.com>
2016-06-19 16:41:25 -0300

    Some code formatting

--- resources/users.go
+++ resources/users.go
@@ -2,8 +2,8 @@ package resources
 
 import (
 	"github.com/gin-gonic/gin"
-    "github.com/jinzhu/gorm"
-    "github.com/ararog/petshop/models"
+  "github.com/jinzhu/gorm"
+  "github.com/ararog/petshop/models"
 )
 
 type UserResource struct {
@@ -14,7 +14,7 @@ func (u *UserResource) User(c *gin.Context) {
 
 	user, ok := c.MustGet("user").(models.User)
 	if ok {
-    	c.JSON(200, user)
+    c.JSON(200, user)
 	} else {
 		json := gin.H{ "message": "User not found" }
 		c.JSON(404, json)
--- services/users.go
+++ services/users.go
@@ -1,24 +1,24 @@
 package services
 
 import (
-    "github.com/jinzhu/gorm"
-    "github.com/ararog/petshop/models"
+  "github.com/jinzhu/gorm"
+  "github.com/ararog/petshop/models"
 )
 
 func Login(db *gorm.DB, email, password string) (error, models.User) {
-    var user models.User
-    err := db.First(&user, "email = ? AND password = ?", email, password).Error
-    return err, user
+  var user models.User
+  err := db.First(&user, "email = ? AND password = ?", email, password).Error
+  return err, user
 }
 
 func GetUserByEmail(db *gorm.DB, email string) (error, models.User) {
-    var user models.User
-    err := db.First(&user, "email = ?", email).Error
-    return err, user
+  var user models.User
+  err := db.First(&user, "email = ?", email).Error
+  return err, user
 }
 
 func GetUserById(db *gorm.DB, id int64) (error, models.User) {
-    var user models.User
-    err := db.First(&user, "id = ?", id).Error
-    return err, user
+  var user models.User
+  err := db.First(&user, "id = ?", id).Error
+  return err, user
 }
