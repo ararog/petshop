91c07ad8320b8f9c9e1624d0e3adb63ebbb9fff4
Rogerio Araujo <rogerio.araujo@gmail.com>
2016-06-25 12:54:01 -0300

    Adding support to configuration handling

--- petshop.go
+++ petshop.go
@@ -4,23 +4,18 @@ import (
   "github.com/ararog/petshop/resources"
   "github.com/ararog/petshop/models"
   "github.com/ararog/petshop/services"
+	"github.com/ararog/petshop/config"
   "github.com/appleboy/gin-jwt"
   "github.com/gin-gonic/gin"
+	"github.com/BurntSushi/toml"
   "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/sqlite"
+  _ "github.com/jinzhu/gorm/dialects/postgres"
   "time"
 )
 
 func GetServerEngine(environment string) *gin.Engine {
 
-	databaseName := ""
-	if environment == "production" {
-		databaseName = "production.db"
-	} else {
-		databaseName = "test.db"
-	}
-
-	db, err := gorm.Open("sqlite3", databaseName)
+	db, err := gorm.Open("postgres", databaseName)
   if err != nil {
     panic("failed to connect database")
   }
@@ -31,7 +26,7 @@ func GetServerEngine(environment string) *gin.Engine {
 
   authMiddleware := &jwt.GinJWTMiddleware{
     Realm: "test zone",
-    Key: []byte("petshop42016"),
+    Key: []byte("petshop2016"),
     Timeout: time.Hour,
     MaxRefresh: time.Hour * 24,
     Authenticator: func(userId string, password string, c *gin.Context) (string, bool) {
